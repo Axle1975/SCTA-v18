0ce5b34201796c3a6d81848534fc0ff2922d95ca
Axle1975 <me@here.com>
2019-10-02 22:14:31 +1000

    footprints

---
+++ hook/lua/footprints.lua
@@ -0,0 +1,6 @@
+LOG('SCTA footprints.lua')
+
+SpecFootprints {
+    { Name = 'Commander2x2',   SizeX=2,  SizeZ=2,  Caps=LAND|SEABED, MaxWaterDepth=15, MaxSlope=1.25, Flags=0 },
+    { Name = 'Vehicle3x3',   SizeX=3,  SizeZ=3,  Caps=LAND, MaxWaterDepth=1.5, MaxSlope=0.75, Flags=0 },
+}
