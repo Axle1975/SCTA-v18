e4e4c2aeab8a7bd4936fa697387d24e6a6b4c519
Axle1975 <me@here.com>
2019-10-25 15:55:06 +1100

    bump version, update changelog

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "SCTA"
-uid = "66BCB3BC-F22A-11E9-8193-8ECC65B1963E"
-version = 12
+uid = "0AA1D93C-F6E3-11E9-B23D-C04242644A14"
+version = 13
 description = "Total Annihilation for Supreme Commander! See where it all began, or take a trip down memory lane. With most ARM units (but not as many for CORE). Choose UEF to spawn an Arm commander, cybran spawn Core. Add an Aeon or Sera AI for an OP opponent. Mod by Raevn, ported to FAForever by Axle"
 author = "Raevn"
 url = ""
