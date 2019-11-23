3e5193bec1e7a8e7bfcfb347880def15576b2dc8
Axle1975 <me@here.com>
2019-11-23 17:04:42 +1100

    Add ARMAMPH (arm pelican)

---
+++ units/ARMAMPH/ARMAMPH_Script.lua
@@ -0,0 +1,27 @@
+#ARM Pelican - Amphibious Kbot
+#ARMAMPH
+#
+#Script created by Axle
+
+local TAunit = import('/mods/SCTA/lua/TAunit.lua').TAunit
+local TAweapon = import('/mods/SCTA/lua/TAweapon.lua').TAweapon
+
+ARMAMPH = Class(TAunit) {
+
+	Weapons = {
+		armamph_weapon1 = Class(TAweapon) {
+			OnWeaponFired = function(self)
+				TAweapon.OnWeaponFired(self)
+				TAunit.ShowMuzzleFlare(self, 0.15)
+			end,
+		},
+		armamph_weapon2 = Class(TAweapon) {
+			OnWeaponFired = function(self)
+				TAweapon.OnWeaponFired(self)
+				TAunit.ShowMuzzleFlare(self, 0.15)
+			end,
+		},
+	},
+}
+
+TypeClass = ARMAMPH
