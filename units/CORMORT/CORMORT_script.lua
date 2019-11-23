7792da841c3a2464827ed4c6e105d69e0cc6cf82
Axle1975 <me@here.com>
2019-11-23 17:04:10 +1100

    add CORMORT

---
+++ units/CORMORT/CORMORT_script.lua
@@ -0,0 +1,20 @@
+#ARM Stumpy - Medium Assault Tank
+#ARMSTUMP
+#
+#Blueprint created by Raevn
+
+local TAunit = import('/mods/SCTA/lua/TAunit.lua').TAunit
+local TAweapon = import('/mods/SCTA/lua/TAweapon.lua').TAweapon
+
+CORMORT = Class(TAunit) {
+
+	Weapons = {
+		CORE_MORT = Class(TAweapon) {
+			OnWeaponFired = function(self)
+				TAweapon.OnWeaponFired(self)
+				TAunit.ShowMuzzleFlare(self, 0.15) #check flare time
+			end,
+		},
+	},
+}
+TypeClass = CORMORT
