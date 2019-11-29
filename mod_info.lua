0077222a7273d931ce305031accfaf1acd15a285
Axle1975 <me@here.com>
2019-11-30 08:10:24 +1100

    bump version 18, changelog

--- mod_info.lua
+++ mod_info.lua
@@ -1,7 +1,7 @@
 name = "SCTA"
-uid = "75BC454E-0F80-11EA-9A7E-E5FBF1669EFB"
-version = 17
-description = "Total Annihilation for Supreme Commander! See where it all began, or take a trip down memory lane. Choose UEF to spawn an Arm commander, cybran to spawn Core. Add an Aeon or Sera AI for an opponent.  Mod by Raevn, ported to FAForever by Axle.  v16 adds Core Krogoth, Core Morty and Arm Pelican. v17 contains bugfixes."
+uid = "8E704ECC-12EC-11EA-A6CA-D6DA930038A1"
+version = 18
+description = "Total Annihilation for Supreme Commander! See where it all began, or take a trip down memory lane. Choose UEF to spawn an Arm commander, cybran to spawn Core. Add an Aeon or Sera AI for an opponent.  Mod by Raevn, ported to FAForever by Axle.  v16 adds Core Krogoth, Core Morty and Arm Pelican. v17-v18 bugfixes."
 author = "Raevn"
 url = ""
 icon = "/mods/SCTA/Core_Commander.png"
